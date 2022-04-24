333431748c857a274356f4312290f119e8c0162d
Paolo Giua <paolo.giuaa@gmail.com>
2022-04-24 13:01:43 +0200

    Bug fix

--- react-project/src/components/presentation-card.js
+++ react-project/src/components/presentation-card.js
@@ -9,28 +9,10 @@ import {ReactComponent as MailSVG} from '../images/mail.svg';
 function PresentationCard() {
     return (
         <div className="PresentationCard">
-            <img className="bkg-image" src={bkgImage} alt=""></img>
-            <div className="blur-filter"></div>
-            {/* <div className='content'>
-                <div className="container h-100">
-                    <div className="row h-100 align-items-center">
-                        <div className="col-sm-12 col-lg-6 centered-content">
-                            <div className="presentation">
-                                <p>
-                                    Hi,<br />
-                                    I'm Paolo,<br />
-                                    developer
-                                </p>
-                                <Button text="Contact Me Now" svg={MailSVG}/>
-                            </div>
-                        </div>
-                        <div className="col-sm-12 col-lg-6 centered-content">
-                            <img className="profile-image" src={profileImage} alt=""></img>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
+            <div className="bkg">
+                <img className="image" src={bkgImage} alt=""></img>
+                <div className="blur-filter"></div>
+            </div>
             <div class="section">
                 <div>
                     <div className="item-section">
