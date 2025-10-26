1d4c5155ea74d2a6fe0685bf1741558fbf844b24
Paolo Giua <paolo.giuaa@gmail.com>
2025-10-26 18:18:48 +0100

    Updated site to new version

--- react-project/src/components/rounded-image/rounded-image.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RoundedImage from './rounded-image';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RoundedImage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
