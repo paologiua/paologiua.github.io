f325c8c65c528a4b0aff96e93b886f8f4b5cd57d
Paolo Giua <paolo.giuaa@gmail.com>
2025-10-26 19:09:45 +0100

    Attempt #3 to fix GitHub Pages asset paths

--- _next/static/RoCiJE6iS9LvCle6E_I5H/_buildManifest.js
+++ _next/static/RoCiJE6iS9LvCle6E_I5H/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gh/paologiua/paologiua.github.io@main//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
